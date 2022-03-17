b9a7061d24d342093d878166adca80576f23164d
UnknownSoul <80086109+ksj020603@users.noreply.github.com>
2022-03-17 12:11:36 +0900

    Add files via upload

---
+++ week3/src/week3/BreakEx1.java
@@ -0,0 +1,24 @@
+package week3;
+
+import java.util.Scanner;
+
+public class BreakEx1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("exit를 입력하면 종료합니다.");
+		
+//		while(true) {
+//			System.out.print(">>");
+//			String text = sc.next();
+//			if(text.equals("exit"))
+//				break;
+//		}
+		for(;;) {
+			System.out.print(">>");
+			String text = sc.next();
+			if(text.equals("exit"))
+				break;
+		}
+		System.out.println("종료합니다.");
+	}
+}
