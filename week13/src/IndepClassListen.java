f6b68c3c280b2a7049872930e7eb654682fa69bd
UnknownSoul <80086109+ksj020603@users.noreply.github.com>
2022-06-02 12:03:15 +0900

    Add files via upload

---
+++ week13/src/IndepClassListen.java
@@ -0,0 +1,37 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class IndepClassListen extends JFrame {
+	public IndepClassListen() {
+		setTitle("Action 이벤트 리스너 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		JButton btn = new JButton("Action");
+		c.add(btn);
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JButton b = (JButton)e.getSource();
+				if(b.getText().equals("Action"))
+					b.setText("액션");
+				else
+					b.setText("Action");
+				setTitle(b.getText());
+			}
+		});
+		
+		
+		setSize(250,120);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new IndepClassListen();
+	}
+}
